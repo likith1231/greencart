37629b36258a41b78a69629bb4f77dbf520f8ff7
likith1231 <likithlu3@gmail.com>
2026-02-07 10:12:24 +0530

    ugauywg

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -8,7 +8,7 @@ const BestSeller = () => {
     <div className='mt-16'>
       <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 lg:grid-cols-5 mt-6'>
-        {products.filter((product) => product.inStock !== false).slice(0,5).map((product,index) => (<ProductCard product={product} key={product._id}/>))}
+        {products.filter((product) => product.isStock !== false).slice(0,5).map((product,index) => (<ProductCard product={product} key={product._id}/>))}
         
       </div>
     </div>
