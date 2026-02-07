258316e537878870208c4691a631a32ab10b2806
likith1231 <likithlu3@gmail.com>
2026-02-07 19:31:33 +0530

    Fix: Change product model collection name to 'products'

--- server/models/product.js
+++ server/models/product.js
@@ -10,6 +10,6 @@ const productSchema = new mongoose.Schema({
     isStock: { type: Boolean, default: true },
 }, { timestamps: true });
 
-const Product = mongoose.models.product || mongoose.model("product", productSchema);
+const Product = mongoose.models.Product || mongoose.model("products", productSchema);
 
 export default Product;
