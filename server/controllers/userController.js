b67d443be2eaf9159c50a0fdd72f96d7fd210aad
likith1231 <likithlu3@gmail.com>
2026-02-07 20:13:54 +0530

    Fix: Return token in login response

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -70,7 +70,7 @@ export const login = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
-    return res.json({ success: true, user: { email: user.email, name: user.name } });
+    return res.json({ success: true, token, user: { email: user.email, name: user.name } });
   } catch (error) {
     console.log("error when login User : ", error.message);
     res.json({ success: false, message: error.message });
